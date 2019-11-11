a291ec68021608000e28279f9bf09fa2733753ca
george9 <giorgos3tg7@yahoo.gr>
2019-11-11 19:05:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+1 function ourMap(array) {
+2 // What do we have to hold/track???
+3 TO COMPLETE!
+4 return (function mapWithCallback(fn, callback) {
+5 // If fn is not a function return current Array
+6 TO COMPLETE TOGETHER!
+7 TO COMPLETE TOGETHER!
+8 // map fn to array and save result to current array
+9 TO COMPLETE!
+10 // If callback is a function, execute callback
+11 TO COMPLETE TOGETHER!
+12 TO COMPLETE TOGETHER!
+13 TO COMPLETE! // We have to return something!
+14 })
+15 }
